8c7801f57e405677276cb4b9c4d2f498dcce7022
Vitor Araujo <kvitorrsantos@gmail.com>
2026-02-18 05:05:17 -0300

    feat: initial configurations

---
+++ ingestion/src/main/java/tcc/vitor/pix_dashboard/PixDashboardApplication.java
@@ -0,0 +1,13 @@
+package tcc.vitor.pix_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixDashboardApplication.class, args);
+	}
+
+}
---
+++ ingestion/src/test/java/tcc/vitor/pix_dashboard/PixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package tcc.vitor.pix_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
